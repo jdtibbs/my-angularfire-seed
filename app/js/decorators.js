32cfc188934ad4af39f7f352abb6e477b06f3436
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-11-21 11:58:56 -0600

    remove dependency on user module.

--- app/js/decorators.js
+++ app/js/decorators.js
@@ -6,16 +6,16 @@
  *    <div ng-cloak>Authentication has resolved.</div>
  * </code>
  */
-angular.module('my.decorators', ['my.firebase.factory', 'my.user.factory'])
+angular.module('my.decorators', ['my.firebase.factory'])
         .config(['$provide', function ($provide) {
                 // adapt ng-cloak to wait for auth before it does its magic
-                $provide.decorator('ngCloakDirective', ['$delegate', 'userFactory',
-                    function ($delegate, userFactory) {
+                $provide.decorator('ngCloakDirective', ['$delegate', 'firebaseFactory',
+                    function ($delegate, firebaseFactory) {
                         var directive = $delegate[0];
                         // make a copy of the old directive
                         var _compile = directive.compile;
                         directive.compile = function (element, attr) {
-                            userFactory.firebaseAuth().$waitForAuth().then(function () {
+                            firebaseFactory.auth().$waitForAuth().then(function () {
                                 // after auth, run the original ng-cloak directive
                                 _compile.call(directive, element, attr);
                             });
