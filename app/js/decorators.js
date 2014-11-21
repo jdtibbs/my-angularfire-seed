39ee5a2dac7ad8c4c76f8f210fed64dd54e9780c
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-11-21 11:40:16 -0600

    page is blank - fixed

--- app/js/decorators.js
+++ app/js/decorators.js
@@ -15,7 +15,7 @@ angular.module('my.decorators', ['my.firebase.factory', 'my.user.factory'])
         // make a copy of the old directive
         var _compile = directive.compile;
         directive.compile = function(element, attr) {
-          userFactory.getAuth().then(function() {
+          userFactory.firebaseAuth().$waitForAuth().then(function() {
             // after auth, run the original ng-cloak directive
             _compile.call(directive, element, attr);
           });
