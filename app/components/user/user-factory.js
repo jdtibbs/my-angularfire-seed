832375df4c56f72ad80c0b29688250bafb846cdb
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-11-21 20:31:34 -0600

    user-factory refactor to show IDE

--- app/components/user/user-factory.js
+++ app/components/user/user-factory.js
@@ -2,11 +2,11 @@
 
 angular.module('my.user.factory', ['my.firebase.factory'])
 
-        .factory('userFactory', ['firebaseFactory', 'changeEmailFactory', '$q', function (firebaseFactory, changeEmailFactory, $q) {
+        .factory('userFactory', ['firebaseFactory', 'changeEmailFactory', '$q', '$timeout', function (firebaseFactory, changeEmailFactory, $q, $timeout) {
                 var INVALID_LOGIN = 'The specified email and or password is incorrect.';
                 var fbauth = firebaseFactory.auth();
 
-                var factory = {
+                var userFactory = {
                     firebaseAuth: function () {
                         return fbauth;
                     },
@@ -40,7 +40,7 @@ angular.module('my.user.factory', ['my.firebase.factory'])
                     },
                     getUid: function () {
                         var def = $q.defer();
-                        factory.getAuth().then(function (auth) {
+                        userFactory.getAuth().then(function (auth) {
                             def.resolve(auth.uid);
                         }).catch(function (error) {
                             def.reject(error);
@@ -51,14 +51,14 @@ angular.module('my.user.factory', ['my.firebase.factory'])
                         fbauth.$unauth();
                     },
                     createAccount: function (profile, pass) {
-                        return factory.createUser(profile.email, pass)
+                        return userFactory.createUser(profile.email, pass)
                                 .then(function () {
                                     // authenticate so we have permission to write to Firebase
-                                    return factory.login(profile.email, pass);
+                                    return userFactory.login(profile.email, pass);
                                 })
                                 .then(function (user) {
                                     // store user data in Firebase after creating account
-                                    return factory.createUserProfile(user.uid, profile).then(function () {
+                                    return userFactory.createUserProfile(user.uid, profile).then(function () {
                                         return user;
                                     });
                                 });
@@ -105,10 +105,10 @@ angular.module('my.user.factory', ['my.firebase.factory'])
                     changeEmail: function (password, newEmail) {
                         var def = $q.defer();
                         // TODO is this the only way to nest these calls!?
-                        factory.getAuth().then(function (auth) {
+                        userFactory.getAuth().then(function (auth) {
                             return auth;
                         }).then(function (auth) {
-                            changeEmailFactory(password, auth.password.email, newEmail, factory).then(function (auth) {
+                            changeEmailFactory.changeEmail(password, auth.password.email, newEmail, userFactory).then(function (auth) {
                                 def.resolve(auth);
                             }).catch(function (error) {
                                 def.reject(error);
@@ -158,100 +158,107 @@ angular.module('my.user.factory', ['my.firebase.factory'])
                     }
                 };
 
-                return factory;
+                return userFactory;
             }])
         .factory('changeEmailFactory', ['firebaseFactory', '$q', function (firebaseFactory, $q) {
-                return function (password, oldEmail, newEmail, userFactory) {
-                    var ctx = {old: {email: oldEmail}, curr: {email: newEmail}};
-                    var oldProfile = {};
-                    var newProfile = {};
-                    // execute activities in order; first we authenticate the user
-                    return authOldAccount()
-                            // then we fetch old account details
-                            .then(loadOldProfile)
-                            // then we create a new account, copying old into the new.
-                            .then(createNewAccount)
-                            // and once they safely exist, then we can delete the old ones
-                            // we have to authenticate as the old user again
-                            .then(authOldAccount)
-                            .then(removeOldProfile)
-                            .then(removeOldLogin)
-                            // and now authenticate as the new user
-                            .then(authNewAccount)
-                            .catch(function (err) {
-                                console.error(err);
-                                return $q.reject(err);
-                            });
-                    function authOldAccount() {
-                        // return 
-                        var def = $q.defer();
-                        userFactory.login(ctx.old.email, password)
-                                .then(function (user) {
-                                    ctx.old.uid = user.uid;
-                                    console.log('authOldAccount ctx.old.uid ' + ctx.old.uid);
-                                    def.resolve();
-                                })
+                var changeEmailFactory = {
+                    changeEmail: function (password, oldEmail, newEmail, userFactory) {
+                        var ctx = {old: {email: oldEmail}, curr: {email: newEmail}};
+                        var oldProfile = {};
+                        var newProfile = {};
+
+                        // execute activities in order; first we authenticate the user
+                        return authOldAccount()
+                                // then we fetch old account details
+                                .then(loadOldProfile)
+                                // then we create a new account, copying old into the new.
+                                .then(createNewAccount)
+                                // and once they safely exist, then we can delete the old ones
+                                // we have to authenticate as the old user again
+                                .then(authOldAccount)
+                                .then(removeOldProfile)
+                                .then(removeOldLogin)
+                                // and now authenticate as the new user
+                                .then(authNewAccount)
                                 .catch(function (err) {
-                                    console.error('authOldAccount ' + err);
-                                    def.reject(err);
+                                    console.error(err);
+                                    return $q.reject(err);
                                 });
-                        return def.promise;
-                    }
 
-                    function loadOldProfile() {
-                        var def = $q.defer();
-                        ctx.old.ref = firebaseFactory.ref('users', ctx.old.uid);
-                        ctx.old.ref.once('value',
-                                function (snap) {
-                                    var dat = snap.val();
-                                    if (dat === null) {
-                                        def.reject(oldEmail + ' not found');
-                                    }
-                                    else {
-                                        oldProfile = dat;
+                        function authOldAccount() {
+                            // return 
+                            var def = $q.defer();
+                            userFactory.login(ctx.old.email, password)
+                                    .then(function (user) {
+                                        ctx.old.uid = user.uid;
+                                        console.log('authOldAccount ctx.old.uid ' + ctx.old.uid);
                                         def.resolve();
-                                    }
-                                },
-                                function (err) {
-                                    def.reject(err);
-                                });
-                        return def.promise;
-                    }
+                                    })
+                                    .catch(function (err) {
+                                        console.error('authOldAccount ' + err);
+                                        def.reject(err);
+                                    });
+                            return def.promise;
+                        }
 
-                    function createNewAccount() {
-                        newProfile = oldProfile;
-                        newProfile.email = ctx.curr.email;
-                        return userFactory.createAccount(newProfile, password).then(function (user) {
-                            ctx.curr.uid = user.uid;
-                            console.log('createNewAccount ctx.curr.uid ' + ctx.curr.uid);
-                        });
-                    }
+                        function loadOldProfile() {
+                            var def = $q.defer();
+                            ctx.old.ref = firebaseFactory.ref('users', ctx.old.uid);
+                            ctx.old.ref.once('value',
+                                    function (snap) {
+                                        var dat = snap.val();
+                                        if (dat === null) {
+                                            def.reject(oldEmail + ' not found');
+                                        }
+                                        else {
+                                            oldProfile = dat;
+                                            def.resolve();
+                                        }
+                                    },
+                                    function (err) {
+                                        def.reject(err);
+                                    });
+                            return def.promise;
+                        }
 
-                    function removeOldProfile() {
-                        var d = $q.defer();
-                        ctx.old.ref.remove(function (err) {
-                            if (err) {
-                                d.reject(err);
-                            } else {
-                                d.resolve();
-                            }
-                        });
-                        return d.promise;
-                    }
+                        function createNewAccount() {
+                            newProfile = oldProfile;
+                            newProfile.email = ctx.curr.email;
+                            return userFactory.createAccount(newProfile, password).then(function (user) {
+                                ctx.curr.uid = user.uid;
+                                console.log('createNewAccount ctx.curr.uid ' + ctx.curr.uid);
+                            });
+                        }
 
-                    function removeOldLogin() {
-                        var def = $q.defer();
-                        userFactory.removeUser(ctx.old.email, password).then(function () {
-                            def.resolve();
-                        }, function (err) {
-                            def.reject(err);
-                        });
-                        return def.promise;
-                    }
+                        function removeOldProfile() {
+                            var d = $q.defer();
+                            ctx.old.ref.remove(function (err) {
+                                if (err) {
+                                    d.reject(err);
+                                } else {
+                                    d.resolve();
+                                }
+                            });
+                            return d.promise;
+                        }
 
-                    function authNewAccount() {
-                        return userFactory.login(ctx.curr.email, password);
-                    }
-                };
+                        function removeOldLogin() {
+                            return userFactory.removeUser(ctx.old.email, password);
+                            /*
+                             var def = $q.defer();
+                             userFactory.removeUser(ctx.old.email, password).then(function () {
+                             def.resolve();
+                             }, function (err) {
+                             def.reject(err);
+                             });
+                             return def.promise;
+                             */
+                        }
+
+                        function authNewAccount() {
+                            return userFactory.login(ctx.curr.email, password);
+                        }
+                    }};
+                return changeEmailFactory;
             }]);
 
