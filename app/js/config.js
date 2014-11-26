2f5d64fb470396791cf572a64b0f30b0c3333a18
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-11-26 11:38:18 -0600

    added version number.

--- app/js/config.js
+++ app/js/config.js
@@ -1,25 +1,31 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp.config', [])
+angular.module('my.config', [])
 
-  // version of this seed app is compatible with angularFire 0.6
-  // see tags for other versions: https://github.com/firebase/angularFire-seed/tags
-  .constant('version', '0.8.2')
+        // version of this seed app is compatible with angularFire 0.6
+        // see tags for other versions: https://github.com/firebase/angularFire-seed/tags
+        .constant('seed.version', '0.8.2')
+        // jdtibbs: version x.y.z
+        // Where:
+        // x = main version number, 1-~.
+        // y = feature number, 0-9. Increase this number if the change contains new features with or without bug fixes.
+        // z = hotfix number, 0-~. Increase this number if the change only contains bug fixes.
+        .constant('version', '0.0.1')
 
-  // where to redirect users if they need to authenticate (see routeSecurity.js)
-  .constant('loginRedirectPath', '/login')
+        // where to redirect users if they need to authenticate (see routeSecurity.js)
+        .constant('loginRedirectPath', '/components/login/login')
 
-  // your Firebase data URL goes here, no trailing slash
-  .constant('FBURL', 'https://INSTANCE.firebaseio.com')
+        // your Firebase data URL goes here, no trailing slash
+        .constant('FBURL', 'https://INSTANCE.firebaseio.com')
 
-  // double check that the app has been configured before running it and blowing up space and time
-  .run(['FBURL', '$timeout', function(FBURL, $timeout) {
-    if( FBURL.match('//INSTANCE.firebaseio.com') ) {
-      angular.element(document.body).html('<h1>Please configure app/js/config.js before running!</h1>');
-      $timeout(function() {
-        angular.element(document.body).removeClass('hide');
-      }, 250);
-    }
-  }]);
+        // double check that the app has been configured before running it and blowing up space and time
+        .run(['FBURL', '$timeout', function (FBURL, $timeout) {
+                if (FBURL.match('//INSTANCE.firebaseio.com')) {
+                    angular.element(document.body).html('<h1>Please configure app/js/config.js before running!</h1>');
+                    $timeout(function () {
+                        angular.element(document.body).removeClass('hide');
+                    }, 250);
+                }
+            }]);
 
