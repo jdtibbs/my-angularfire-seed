efb1affcd5dadf34094129713e12e3143afea9b2
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-12-01 11:46:24 -0600

    auto-focus - added

--- app/app.js
+++ app/app.js
@@ -18,4 +18,15 @@ angular.module('my.app', [
     'ngRoute'
 ])
 
-        
+        .directive('autoFocus', function ($timeout) {
+            return {
+                restrict: 'AC',
+                link: function (_scope, _element) {
+                    $timeout(function () {
+                        _element[0].focus();
+                    }, 0);
+                }
+            };
+        });
+
+
