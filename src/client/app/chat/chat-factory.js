8b14a3ead9f1d70d5ba7e0fa05c20fe17a8cabc5
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-12-17 13:13:27 -0600

    per style guide - backout

--- src/client/app/chat/chat-factory.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('my.chat.factory', [])
-
-        .factory('message', ['firebaseFactory', function (firebaseFactory) {
-                return firebaseFactory.syncArray('messages', {limitToLast: 10, endAt: null});
-            }]);
