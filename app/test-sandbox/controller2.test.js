21f5e9e024f1dc9ba3d5a9b72cf6c8deee954ae0
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-12-18 14:10:24 -0600

    test-sandbox - new test

---
+++ app/test-sandbox/controller2.test.js
@@ -0,0 +1,50 @@
+angular.module('app.spat', []);
+
+angular.module('app.spat')
+        .factory('service', Service);
+
+function Service() {
+    var factory = {
+        hello: function () {
+            return 'Hello Sam';
+        }
+    };
+    return factory;
+}
+
+angular.module('app.spat')
+        .controller('controller', Controller);
+
+Controller.$inject = ['service'];
+
+function Controller(service) {
+    var vm = this;
+    vm.hello = hello;
+    function hello() {
+        return service.hello();
+    }
+}
+
+describe('app.spat test', function () {
+    'use strict';
+
+    var controller;
+
+    beforeEach(
+            module('app.spat')
+            );
+
+    beforeEach(inject(function ($injector) {
+        var $controller = $injector.get('$controller');
+        controller = $controller('controller');
+    }));
+
+    it('controller should be defined', function () {
+        expect(controller).toBeDefined();
+    });
+
+    it('should say Hello Sam', function () {
+        expect(controller.hello()).toEqual('Hello Sam');
+    });
+
+});
