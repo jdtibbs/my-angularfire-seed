2184b3819dea9c56b561fbccf585ae417957e1ea
Jack Tibbs <jdtibbs@users.noreply.github.com>
2014-12-26 05:21:20 -0600

    menu - remove unused function

--- app/components/menu/menu.controller.js
+++ app/components/menu/menu.controller.js
@@ -13,7 +13,6 @@
         vm.toggleCollapse = toggleCollapse;
         vm.logout = logout;
         vm.items = items();
-        vm.hasItems = hasItems;
 
         function toggleCollapse() {
             vm.collapse = !vm.collapse;
@@ -27,20 +26,6 @@
 
         function items() {
             return menuFactory.menu();
-        }
-
-        function hasItems(items) {
-            console.log(JSON.stringify(items));
-            console.log((items.length === 0));
-            angular.forEach(items, function (value, key) {
-                console.log('value = ' + JSON.stringify(value.value) + ' ' + JSON.stringify(value.url));
-                if (value.items) {
-                    angular.forEach(value.items, function (value, key) {
-                        console.log('\t' + 'value = ' + JSON.stringify(value.value) + ' ' + JSON.stringify(value.url));
-                    });
-                }
-            });
-            return (items.length === 0);
         }
     }
 })();
